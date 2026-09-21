3f39f2443bf9cd59d1d84f280f6430568f107762
kinanti nailah <kinanti.nailah06@gmail.com>
2026-09-21 14:43:07 +0700

    tambahKonsultasi

---
+++ RumahSakit/Konsultasi.java
@@ -0,0 +1,43 @@
+import java.time.LocalDateTime;
+import java.util.arrayList;
+
+public class Konsultasi {
+    private LocalDate tanggal;
+    private Pegawai dokter;
+    private Pegawai perawat;
+
+    public LocalDate getTanggal(){
+        return tanggal;
+    }
+
+    public void setTangal(LocalDate tanggal){
+        this.tanggal = tanggal;
+    }
+
+    public Pegawai getDokter() {
+        return dokter;
+    }
+
+    public void setDokter(Pegawai dokter) {
+        this.dokter = dokter;
+    }
+
+    public Pegawai getPerawat() {
+        return perawat;
+    }
+
+    public void setPerawat(Pegawai perawat) {
+        this.perawat = perawat;
+    }
+
+    public String getInfo(){
+        String info = "";
+        info += "\tTanggal: " +tanggal;
+        info += ", Dokter: " + dokter.getInfo();
+        info += ", Perawat: " + perawat.getInfo();
+        info += "\n";
+
+        return info;
+
+    }
+}
